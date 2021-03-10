b06cddc28f40364c29eaf3f9f84bbdddaf60440d
harsh <harshdev808@gmail.com>
2021-03-10 10:40:24 +0530

    Added code for passing test 4 of Custom Delimiter

--- src/main/java/stringCalculator/StringCalculator.java
+++ src/main/java/stringCalculator/StringCalculator.java
@@ -3,7 +3,14 @@ package stringCalculator;
 public class StringCalculator {
 	public int Add(String numbers) {
 		numbers=numbers.trim();
-		String num[]=numbers.split(",|\n",0);
+		String delimiter=",|\n";
+		if(numbers.length()!=0 && numbers.charAt(0)=='/')
+		{
+			String number[]=numbers.split("\n",2);
+			delimiter=number[0].substring(2);
+			numbers=number[1];
+		}
+		String num[]=numbers.split(delimiter,0);
 		int sum=0;
 		for(int i=0;i<num.length;i++)
 			if(num[i].length()!=0)
