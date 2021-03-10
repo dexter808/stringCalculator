28949314886753fba3a65e3da703b7e1bdee5cfb
harsh <harshdev808@gmail.com>
2021-03-10 09:39:59 +0530

    Refactored code for passing test 1

--- src/main/java/stringCalculator/StringCalculator.java
+++ src/main/java/stringCalculator/StringCalculator.java
@@ -2,6 +2,12 @@ package stringCalculator;
 
 public class StringCalculator {
 	public int Add(String numbers) {
-		return 0;
+		numbers=numbers.trim();
+		String num[]=numbers.split(",",0);
+		if(numbers.length()==0)
+			return 0;
+		if(num.length==1)
+			return Integer.parseInt(num[0]);
+		return Integer.parseInt(num[0])+Integer.parseInt(num[1]);
 	}
 }
