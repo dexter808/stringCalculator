7e9535d4299d2222c2852fade8bdf8fea808b6f5
harsh <harshdev808@gmail.com>
2021-03-10 10:03:36 +0530

    Added code for test 3

--- src/main/java/stringCalculator/StringCalculator.java
+++ src/main/java/stringCalculator/StringCalculator.java
@@ -3,11 +3,7 @@ package stringCalculator;
 public class StringCalculator {
 	public int Add(String numbers) {
 		numbers=numbers.trim();
-		String num[]=numbers.split(",",0);
-		if(numbers.length()==0)
-			return 0;
-		if(num.length==1)
-			return Integer.parseInt(num[0]);
+		String num[]=numbers.split(",|\n",0);
 		int sum=0;
 		for(int i=0;i<num.length;i++)
 			if(num[i].length()!=0)
