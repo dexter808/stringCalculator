c1ffe1a9ef8e0d729ff0b792b02a826a66f8a652
harsh <harshdev808@gmail.com>
2021-03-10 11:02:24 +0530

    Added code for test 5 to throw exception for negative numbers

--- src/main/java/stringCalculator/StringCalculator.java
+++ src/main/java/stringCalculator/StringCalculator.java
@@ -1,7 +1,7 @@
 package stringCalculator;
 
 public class StringCalculator {
-	public int Add(String numbers) {
+	public int Add(String numbers) throws Exception{
 		numbers=numbers.trim();
 		String delimiter=",|\n";
 		if(numbers.length()!=0 && numbers.charAt(0)=='/')
@@ -11,10 +11,24 @@ public class StringCalculator {
 			numbers=number[1];
 		}
 		String num[]=numbers.split(delimiter,0);
+		boolean hasNegatives=false;
+		String messageForNegativeNumbers="negatives not allowed :";
 		int sum=0;
 		for(int i=0;i<num.length;i++)
 			if(num[i].length()!=0)
-			sum+=Integer.parseInt(num[i]);
+			{
+				int n=Integer.parseInt(num[i]);
+				if(n<0)
+				{
+					hasNegatives=true;
+					messageForNegativeNumbers+=" "+n;
+				}
+				sum+=n;
+			}
+		if(hasNegatives)
+		{
+			throw new Exception(messageForNegativeNumbers);
+		}
 		return sum;
 	}
 }
--- src/test/java/stringCalculator/StringCalculatorTest.java
+++ src/test/java/stringCalculator/StringCalculatorTest.java
@@ -15,33 +15,33 @@ class StringCalculatorTest {
 	}
 	@Test
 	@DisplayName("Testing For Upto 2 numbers(Part 1)")
-	void testAddForUpto2numbers() {
+	void testAddForUpto2numbers() throws Exception {
 		assertEquals(0, stringCalculator.Add(""),"Sum of empty string should be 0");
 		assertEquals(12, stringCalculator.Add("12"),"Sum of 1 element should be the same as that element");
 		assertEquals(7, stringCalculator.Add("4,3"),"Sum of 3,4 = 7");
 	}
 	@Test
 	@DisplayName("Testing for unknown aount of numbers")
-	void testAddForUnknownAmtOfNum() {
+	void testAddForUnknownAmtOfNum() throws Exception {
 		assertEquals(15, stringCalculator.Add("1,2,,3,4,5"),"Adding unknown amount of numbers");
 		assertEquals(3, stringCalculator.Add("1,1,1"));
 	}
 	
 	@Test
 	@DisplayName("Testing for line change as a delimeter")
-	void testAddWithLineChangeAlsoAsDelimeter() {
+	void testAddWithLineChangeAlsoAsDelimeter() throws Exception {
 		assertEquals(3, stringCalculator.Add("2,\n1"));
 		assertEquals(5, stringCalculator.Add("3,\n1,1,"));
 	}
 	@Test
 	@DisplayName("Testing for custom delimeters")
-	void testAddForCustomDelimiter() {
+	void testAddForCustomDelimiter() throws Exception {
 		assertEquals(6, stringCalculator.Add("//;\n4;2"));
 		assertEquals(12, stringCalculator.Add("//!\n4!8"));
 	}
 	@Test
 	@DisplayName("Testing for negative numbers not allowed")
-	void testAddForNegativeNumbers() {
+	void testAddForNegativeNumbers() throws Exception{
 		assertThrows(Exception.class, () -> stringCalculator.Add("1,2,3\n,-6"),"Expected to throw exception for negative numbers but it did not");
 		assertThrows(Exception.class, () -> stringCalculator.Add("-3"));
 	}
